dcfc9d7d692c28eb2cb27469e4669e995b91bcb3
koheihida <88849254+koheihida@users.noreply.github.com>
2021-09-29 16:05:32 +0900

    ユーザー機能追加修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,          presence: true, format: { with: /\A[a-zA-Z]+\z/}
-  validates :email,             format: { with:/\A[^@\s]+@[^@\s]+\z/}
+  validates :password,             format: { with:/\A[^@\s]+@[^@\s]+\z/}
   validates :first_name,        presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :family_name,       presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :first_name_kana,   presence: true, format: { with:/\A[ァ-ヶー－]+\z/ }
