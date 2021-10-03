c52c78beea858009a77f8b663bfc3006ec375147
koheihida <88849254+koheihida@users.noreply.github.com>
2021-10-03 21:24:57 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :move_to_index, except: [:index, :show]
+
   def index
     @items = Item.all.order(created_at: :desc)
   end
@@ -21,10 +23,35 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end 
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+  end
+
+  # def destroy
+  #   @item = Item.find(item_params[:id])
+  #   @item.destroy
+  # end
+
   private
 
   def item_params
     params.require(:item).permit(:image, :goode_name, :explanation, :category_id, :detail_id, :postage_id, :prefecture_id,
                                  :period_date_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
