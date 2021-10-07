07799b9ee046cdee947361c38c35585425b57fc9
koheihida <88849254+koheihida@users.noreply.github.com>
2021-10-07 18:27:47 +0900

    デプロイ時修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("../item_price")
 require("../card")
-require("../my_1.js")
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
