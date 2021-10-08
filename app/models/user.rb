a6f87a9ff843c40ae2729b3bff349887c9395ce4
koheihida <88849254+koheihida@users.noreply.github.com>
2021-10-08 21:04:15 +0900

    SNS連動

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,23 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
+  has_many :buys
+  has_many :items
+  has_many :sns_credentials
+
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    user = User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+        email: auth.info.email
+    )
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    { user: user, sns: sns }
+  end
 
   validates :nickname,          presence: true, format: { with: /\A[a-zA-Z]+\z/ }
   validates :password,          format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/ }
@@ -12,6 +28,4 @@ class User < ApplicationRecord
   validates :family_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday,          presence: true
 
-  has_many :buys
-  has_many :items
 end
